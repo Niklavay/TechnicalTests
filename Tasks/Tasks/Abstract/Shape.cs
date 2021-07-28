c297fbe5ea9a4801d71e3a9adea52f39966c8d80
Mykola Bodnar <kolyakepik@gmail.com>
2021-07-28 17:24:33 +0300

    Made functinality of abstract class

--- Tasks/Tasks/Abstract/Shape.cs
+++ Tasks/Tasks/Abstract/Shape.cs
@@ -1,7 +1,16 @@
+using System;
+
 namespace Tasks.Abstract
 {
-    public class Shape
+    public abstract class Shape : IComparable<Shape>
     {
-        
+        protected abstract double Area { get; set; }
+
+        public int CompareTo(Shape that)
+        {
+            if (this.Area <  that.Area) return -1;
+            if (this.Area == that.Area) return 0;
+            return 1;
+        }
     }
 }
